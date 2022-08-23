d9fdf1821b6316128257f2bbae7f51c678bfaaba
vaibhav <ajmera.vaibhav919@gmail.com>
2022-08-23 15:48:46 +0530

    After getting response from retrofit

--- app/src/main/java/com/example/githubtrendingrepoapp/MainActivity.kt
+++ app/src/main/java/com/example/githubtrendingrepoapp/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.githubtrendingrepoapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 //import com.google.gson.internal.GsonBuildConfig
 import retrofit2.Call
 import retrofit2.Callback
@@ -14,13 +16,15 @@ import retrofit2.converter.gson.GsonConverterFactory
 class MainActivity : AppCompatActivity() {
 
     val modelList=ArrayList<Model>()
+    //val responseBody=Response<List<APIResponseObject>>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        getMyData();
-        initData()
-        setRecyclerView()
+
+        getMyData()
+       // initData()
+        //setRecyclerView()
     }
 
     private fun getMyData() {
@@ -34,12 +38,26 @@ class MainActivity : AppCompatActivity() {
             ) {
                 if(response!=null){
                     val responseBody= response.body()
-
+                    val imageView:ImageView?=findViewById(R.id.image_view)
                     if (responseBody != null) {
                         for(data in responseBody){
+                            //val imageView:ImageView=view
+                            modelList.add(Model(data.author.toString(),data.name.toString(),data.description.toString(),R.drawable.ic_action_name))
+//                           if(imageView!=null){
+//                               Picasso.get().load(data.avatar.toString()).into(imageView)
+//
+//                           }
+
                             Log.d("Author is ",data.author.toString())
                             Log.d("Name is ",data.name.toString())
+
+
                         }
+                        val recyclerAdapter=RecyclerAdapter(modelList)
+                        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView);
+
+                        recyclerView.adapter=recyclerAdapter
+                        recyclerView.setHasFixedSize(true)
                     }
                 }
             }
@@ -59,17 +77,17 @@ class MainActivity : AppCompatActivity() {
         recyclerView.adapter=recyclerAdapter
         recyclerView.setHasFixedSize(true)
     }
-    private fun initData(){
-
-        modelList.add(Model("vajmera","hellokotlin","The details are to be shown here only",R.drawable.ic_action_name))
-        modelList.add(Model("vajmera","hellokotlin","The details are to be shown here only",R.drawable.ic_action_name))
-        modelList.add(Model("vajmera","hellokotlin","The details are to be shown here only",R.drawable.ic_action_name))
-        modelList.add(Model("vajmera","hellokotlin","The details are to be shown here only",R.drawable.ic_action_name))
-        modelList.add(Model("vajmera","hellokotlin","The details are to be shown here only",R.drawable.ic_action_name))
-        modelList.add(Model("vajmera","hellokotlin","The details are to be shown here only",R.drawable.ic_action_name))
-        modelList.add(Model("vajmera","hellokotlin","The details are to be shown here only",R.drawable.ic_action_name))
-
-
-
-    }
+//    private fun initData(){
+//
+//        modelList.add(Model("vajmera","hellokotlin","The details are to be shown here only",R.drawable.ic_action_name))
+//        modelList.add(Model("vajmera","hellokotlin","The details are to be shown here only",R.drawable.ic_action_name))
+//        modelList.add(Model("vajmera","hellokotlin","The details are to be shown here only",R.drawable.ic_action_name))
+//        modelList.add(Model("vajmera","hellokotlin","The details are to be shown here only",R.drawable.ic_action_name))
+//        modelList.add(Model("vajmera","hellokotlin","The details are to be shown here only",R.drawable.ic_action_name))
+//        modelList.add(Model("vajmera","hellokotlin","The details are to be shown here only",R.drawable.ic_action_name))
+//        modelList.add(Model("vajmera","hellokotlin","The details are to be shown here only",R.drawable.ic_action_name))
+//
+//
+//
+//    }
 }
